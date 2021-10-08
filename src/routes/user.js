fcf1e99ca391bbd00789754c923b8e0a27beb78d
Izzy Smillie <isabelle.smillie@gmail.com>
2021-10-08 14:48:46 +0100

    Add get users & create new user functionality

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,12 +3,29 @@ const { apiUserName, apiSecretkey } = require('../config')
 
 // Get all Users
 function getUsers(req, res) {
-  console.log('getting user')
+  const userQuery = UserModel.find({})
+
+  userQuery.exec((err, user) => {
+    if (err) {
+      return res.status(400).send(err)
+    }
+
+    res.status(200).send(user)
+  })
 }
 
 // Create new user
-function postUser(req, res) {
-  console.log('create new user')
+async function postUser(req, res) {
+  console.log(req.body)
+  const newuser = await new UserModel(req.body)
+
+  newuser.save((err, user) => {
+    if (err) {
+      return res.status(400).send(err)
+    } else {
+      res.status(200).send({ message: 'User added successfully', user })
+    }
+  })
 }
 
 // Get specific user by Id
