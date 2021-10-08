270bc7067cf9b59d8bf90fcbfd6b48eaf58800b6
Izzy Smillie <isabelle.smillie@gmail.com>
2021-10-08 16:00:30 +0100

    Add delete tests

--- src/tests/user.js
+++ src/tests/user.js
@@ -76,4 +76,65 @@ describe('Users', () => {
         })
     })
   })
+
+  describe('/DELETE/:id user', () => {
+    it('should NOT DELETE a user without auth', (done) => {
+      let user = new UserModel({
+        username: 'Smoking man',
+        email: 'cigs@man.com',
+        password: 'Trust no one',
+      })
+      user.save((err, user) => {
+        chai
+          .request(server)
+          .delete('/user/' + user.id)
+          .end((err, res) => {
+            res.should.have.status(401)
+            res.body.should.be.a('object')
+            res.body.should.have.property('message').eql('Not Authorized')
+            done()
+          })
+      })
+    })
+    it('should NOT DELETE a user with wrong credentials', (done) => {
+      let user = new UserModel({
+        username: 'Walter Skinner',
+        email: 'walter@skinner.com',
+        password: 'That will be all',
+      })
+      user.save((err, user) => {
+        chai
+          .request(server)
+          .delete('/user/' + user.id)
+          .auth('blackberry', 'pie')
+          .end((err, res) => {
+            res.should.have.status(401)
+            res.body.should.be.a('object')
+            res.body.should.have.property('message').eql('Not Authorized')
+            done()
+          })
+      })
+    })
+    it('should DELETE a user by the given id with auth', (done) => {
+      let user = new UserModel({
+        username: 'John Dogget',
+        email: 'john@dogget.com',
+        password: 'Monica Reyes',
+      })
+      user.save((err, user) => {
+        chai
+          .request(server)
+          .delete('/user/' + user.id)
+          .auth(apiUserName, apiSecretkey)
+          .end((err, res) => {
+            res.should.have.status(200)
+            res.body.should.be.a('object')
+            res.body.should.have
+              .property('message')
+              .eql('User deleted successfully')
+            done()
+          })
+      })
+    })
+  })
 })
